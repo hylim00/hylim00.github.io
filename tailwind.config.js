ca9cb0109fb2676f27cdbc4aab64fffde0b6ee60
hylim00 <165804972+hylim00@users.noreply.github.com>
2024-05-22 20:48:21 +0700

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#a855f7",
+        dark: "#0f172a",
+      },
+    },
+  },
+  plugins: [],
+};
